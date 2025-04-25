c20ddb7b0bf62e13fe55a75055c2f7887ccf5d47
Yashwanth_kumar_k <104084611+yeshcrik@users.noreply.github.com>
2025-04-25 23:33:14 +0530

    Update index.js
    Signed-off-by: Yashwanth_kumar_k <104084611+yeshcrik@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('Hello from Jenkins Pipeline!'));
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
